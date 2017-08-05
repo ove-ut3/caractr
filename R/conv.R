edb58f1c351c6c8e575908c1ae881e96883f5547
Stephane Le Henanff <github@le-henanff.fr>
2017-08-05 14:09:17 +0200

    Ajout d'un paramètre à la fonction conv_nombre_toutes_lettres : obtenir le libellé d'itération (2 = deuxième)

--- R/conv.R
+++ R/conv.R
@@ -1,13 +1,15 @@
 #' conv_nombre_toutes_lettres
 #'
-#' @param nombre.
+#' @param nombre \dots
+#' @param type \dots
+#' @param langue \dots
 #'
 #' @export
-conv_nombre_toutes_lettres <- function(nombre, langue = "fr") {
+conv_nombre_toutes_lettres <- function(nombre, type = "lettre", langue = "fr") {
 
   conv_nombre_toutes_lettres <- dplyr::tibble(nombre) %>%
     dplyr::left_join(caractr::data_mots_lettres, by = "nombre") %>%
-    dplyr::pull(lettres)
+    .[[type]]
 
   return(conv_nombre_toutes_lettres)
 }
--- R/data.R
+++ R/data.R
@@ -6,6 +6,13 @@ data <- function() {
   data_mots_vides <- importr::importer_table_access("mots_vides", paste0(racine_packages, "caractr/Tables_ref.accdb"))
   save("data_mots_vides", file = paste0(racine_packages, "caractr/data/data_mots_vides.RData"))
 
-  data_mots_lettres <- importr::importer_table_access("mots_lettres", paste0(racine_packages, "caractr/Tables_ref.accdb"))
+  data_mots_lettres <- importr::importer_table_access("mots_lettres", paste0(racine_packages, "caractr/Tables_ref.accdb")) %>%
+    dplyr::mutate(ieme = paste0(lettres, "ième"),
+                  ieme = ifelse(nombre == 0, NA_character_, ieme),
+                  ieme = ifelse(nombre == 1, "premier", ieme),
+                  ieme = stringr::str_replace(ieme, "eième$", "ième"),
+                  ieme = stringr::str_replace(ieme, "qième$", "quième"),
+                  ieme = stringr::str_replace(ieme, "fième$", "vième"),
+                  ieme_f = ifelse(nombre == 1, "première", ieme))
   save("data_mots_lettres", file = paste0(racine_packages, "caractr/data/data_mots_lettres.RData"))
 }
