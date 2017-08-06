4e95042a579ddfcddddcc1e98239eeb9a0dffbb4
Stephane Le Henanff <github@le-henanff.fr>
2017-08-06 08:35:05 +0200

    Déplacement de la base Access dans le répertoire data

--- R/data.R
+++ R/data.R
@@ -3,10 +3,10 @@
 #' @export
 data <- function() {
 
-  data_mots_vides <- importr::importer_table_access("mots_vides", paste0(racine_packages, "caractr/Tables_ref.accdb"))
+  data_mots_vides <- importr::importer_table_access("mots_vides", paste0(racine_packages, "caractr/data/Tables_ref.accdb"))
   save("data_mots_vides", file = paste0(racine_packages, "caractr/data/data_mots_vides.RData"))
 
-  data_mots_lettres <- importr::importer_table_access("mots_lettres", paste0(racine_packages, "caractr/Tables_ref.accdb")) %>%
+  data_mots_lettres <- importr::importer_table_access("mots_lettres", paste0(racine_packages, "caractr/data/Tables_ref.accdb")) %>%
     dplyr::mutate(ieme = paste0(lettres, "ième"),
                   ieme = ifelse(nombre == 0, NA_character_, ieme),
                   ieme = ifelse(nombre == 1, "premier", ieme),
