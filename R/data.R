50d9c9e63cd999ecdfcb58d1468a4037572d7e4c
Stéphane Le Hénanff <slehenanff@ups-tlse.fr>
2017-10-25 13:41:24 +0200

    mots_lettres : ajout des champs "1er" et "1ère"

--- R/data.R
+++ R/data.R
@@ -15,6 +15,9 @@ generer_data <- function() {
                   ieme = stringr::str_replace(ieme, "eième$", "ième"),
                   ieme = stringr::str_replace(ieme, "qième$", "quième"),
                   ieme = stringr::str_replace(ieme, "fième$", "vième"),
-                  ieme_f = ifelse(nombre == 1, "première", ieme))
+                  ieme_f = ifelse(nombre == 1, "première", ieme),
+                  ieme_chiffre = ifelse(nombre == 1, "1er", paste0(nombre, "ème")),
+                  ieme_chiffre = ifelse(nombre == 0, NA_character_, ieme_chiffre),
+                  ieme_chiffre_f = ifelse(nombre == 1, "1ère", ieme_chiffre))
   save("mots_lettres", file = paste0(racine_packages, "caractr/data/mots_lettres.RData"))
 }
