8e876b0262a633991cc1d1bf70b359408a17a009
Stéphane Le Hénanff <slehenanff@ups-tlse.fr>
2017-11-17 09:22:39 +0100

    nettoyer_numero_telephone : n'utilise plus le tibble

--- R/coordonnees.R
+++ R/coordonnees.R
@@ -12,14 +12,14 @@
 #' @export
 nettoyer_numero_telephone <- function(numero) {
 
-  nettoyer_numero_telephone <- tibble::tibble(numero) %>%
-    dplyr::mutate(numero = stringr::str_replace_all(numero, "\n", " "),
-                  numero2 = stringr::str_replace_all(numero, "[^\\d]", ""),
-                  numero2 = stringr::str_replace_all(numero2, "^(00)?33(\\d{9})", "0\\2"),
-                  numero2 = ifelse(nchar(numero2) == 9, paste0("0", numero2), numero2),
-                  numero2 = ifelse(nchar(numero2) == 10, stringr::str_replace(numero2, "(\\d{2})(\\d{2})(\\d{2})(\\d{2})(\\d{2})", "\\1 \\2 \\3 \\4 \\5"),
-                            numero))
-  return(nettoyer_numero_telephone$numero2)
+  numero <- stringr::str_replace_all(numero, "\n", " ")
+
+  numero2 <- stringr::str_replace_all(numero, "[^\\d]", "") %>%
+    stringr::str_replace_all("^(00)?33(\\d{9})", "0\\2") %>%
+    { ifelse(nchar(.) == 9, paste0("0", .), .) } %>%
+    { ifelse(nchar(.) == 10, stringr::str_replace(., "(\\d{2})(\\d{2})(\\d{2})(\\d{2})(\\d{2})", "\\1 \\2 \\3 \\4 \\5"), numero) }
+
+  return(numero2)
 }
 
 #' Valider des emails par regex
