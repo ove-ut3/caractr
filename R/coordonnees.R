099376b99e883c464b680e9b696a02b52444bc54
Stéphane Le Hénanff <github@le-henanff.fr>
2018-04-02 17:38:02 +0200

    nettoyer_numero_telephone : mise à jour de la regex

--- R/coordonnees.R
+++ R/coordonnees.R
@@ -15,7 +15,7 @@ nettoyer_numero_telephone <- function(numero) {
   numero <- stringr::str_replace_all(numero, "\n", " ")
 
   numero2 <- stringr::str_replace_all(numero, "[^\\d]", "") %>%
-    stringr::str_replace_all("^(00)?33(\\d{9})", "0\\2") %>%
+    stringr::str_replace_all("^(00)?330?(\\d{9})", "0\\2") %>%
     { ifelse(nchar(.) == 9, paste0("0", .), .) } %>%
     { ifelse(nchar(.) == 10, stringr::str_replace(., "(\\d{2})(\\d{2})(\\d{2})(\\d{2})(\\d{2})", "\\1 \\2 \\3 \\4 \\5"), numero) }
 
