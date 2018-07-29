32d4abcfcc1059c8c76803c580cd1359db90c5bd
Stephane Le Henanff <github@le-henanff.fr>
2018-07-29 18:19:28 +0200

    Anglicisation de conv_nombre_toutes_lettres -> str_conv_number_letter (2)

--- R/conv.R
+++ R/conv.R
@@ -1,7 +1,7 @@
 #' Convert a numeric vector to full letters.
 #'
 #' @param x A numeric vector.
-#' @param type Choice between : lettre|ieme|ieme_chiffre
+#' @param type Choice between : letter|ieme|ieme_number
 #' @param female If \code{TRUE}, female form.
 #' @param language French language only available.
 #'
@@ -10,14 +10,14 @@
 #' caractr::str_conv_number_letter(1, female = TRUE)
 #'
 #' @export
-str_conv_number_letter <- function(x, type = "lettre", female = FALSE, language = "fr") {
+str_conv_number_letter <- function(x, type = "letter", female = FALSE, language = "fr") {
 
   if (female) {
     type <- paste0(type, "_f")
   }
 
   letter <- dplyr::tibble(x) %>%
-    dplyr::left_join(caractr::mots_lettres, by = c("x" = "nombre")) %>%
+    dplyr::left_join(caractr::number_letter, by = "x") %>%
     dplyr::pull(type)
 
   return(letter)
--- R/data.R
+++ R/data.R
@@ -7,17 +7,17 @@ generate_data <- function() {
   stopwords <- impexp::access_importer("stopwords", paste0(racine_packages, "caractr/raw/Tables_ref.accdb"))
   save("stopwords", file = paste0(racine_packages, "caractr/data/stopwords.RData"))
 
-  mots_lettres <- impexp::access_importer("mots_lettres", paste0(racine_packages, "caractr/raw/Tables_ref.accdb")) %>%
-    dplyr::mutate(lettre_f = ifelse(lettre == "un", "une", lettre),
-                  ieme = paste0(lettre, "ième"),
-                  ieme = ifelse(nombre == 0, NA_character_, ieme),
-                  ieme = ifelse(nombre == 1, "premier", ieme),
+  number_letter <- impexp::access_importer("number_letter", paste0(racine_packages, "caractr/raw/Tables_ref.accdb")) %>%
+    dplyr::mutate(letter_f = ifelse(letter == "un", "une", letter),
+                  ieme = paste0(letter, "ième"),
+                  ieme = ifelse(x == 0, NA_character_, ieme),
+                  ieme = ifelse(x == 1, "premier", ieme),
                   ieme = stringr::str_replace(ieme, "eième$", "ième"),
                   ieme = stringr::str_replace(ieme, "qième$", "quième"),
                   ieme = stringr::str_replace(ieme, "fième$", "vième"),
-                  ieme_f = ifelse(nombre == 1, "première", ieme),
-                  ieme_chiffre = ifelse(nombre == 1, "1er", paste0(nombre, "ème")),
-                  ieme_chiffre = ifelse(nombre == 0, NA_character_, ieme_chiffre),
-                  ieme_chiffre_f = ifelse(nombre == 1, "1ère", ieme_chiffre))
-  save("mots_lettres", file = paste0(racine_packages, "caractr/data/mots_lettres.RData"))
+                  ieme_f = ifelse(x == 1, "première", ieme),
+                  ieme_number = ifelse(x == 1, "1er", paste0(x, "ème")),
+                  ieme_number = ifelse(x == 0, NA_character_, ieme_number),
+                  ieme_number_f = ifelse(x == 1, "1ère", ieme_number))
+  save("number_letter", file = paste0(racine_packages, "caractr/data/number_letter.RData"))
 }
