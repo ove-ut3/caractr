0a0a5b5a9be5c430b3e4fd0606353fb70751b9bd
stephLH <github@le-henanff.fr>
2019-07-12 16:30:33 +0200

    gather data documentations

--- R/data-stopwords.R
+++
@@ -1,8 +0,0 @@
-#' A list of stop words.
-#'
-#' @format A data frame with 145 rows and 2 variables:
-#' \describe{
-#'   \item{language}{language code (ISO 639-1)}
-#'   \item{stopword}{stop word}
-#' }
-"stopwords"
--- R/data.R
+++ R/data.R
@@ -11,3 +11,21 @@
 #'   \item{ieme_number_f}{ordinal number in its feminine form}
 #' }
 "number_letter"
+
+#' A list of stop words.
+#'
+#' @format A data frame with 145 rows and 2 variables:
+#' \describe{
+#'   \item{language}{language code (ISO 639-1)}
+#'   \item{stopword}{stop word}
+#' }
+"stopwords"
+
+#' A conversion table between non accentued words and accentuated words in French.
+#'
+#' @format A data frame with 13696 rows and 2 variables:
+#' \describe{
+#'   \item{word}{non accentued word}
+#'   \item{word_fr_accent}{accentued word}
+#' }
+"word_fr_accent"
